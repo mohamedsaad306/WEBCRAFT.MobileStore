63f8508d8cfa26adfa88f622e02c59d76982a4e9
Mohamed <Mohamed@MOHAMED-PC>
2018-12-29 18:20:20 +0200

    add exim

---
+++ exim/src/components/UsersManagement.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>{{title}}</h3>
+</template>
+
+<script>
+export default {
+  name: "UsersManagement",
+  data() {
+    return {
+      title: "This is Users Manager "
+    };
+  }
+};
+</script>
+
+<style>
+</style>
