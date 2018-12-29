1130bded2a4b5d15bf3054674e66680cdb9ea847
Mohamed <Mohamed@MOHAMED-PC>
2018-12-29 18:20:20 +0200

    add exim

---
+++ exim/src/components/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Home : {{Title}}</h2>
+    <b-button :model="pressed" variant="primary">Toggle Me</b-button>
+    
+    <b-alert    
+      variant="danger"
+      dismissible
+      :show="showDismissibleAlert"
+      @dismissed="showDismissibleAlert=false"
+    >Dismissible Alert!</b-alert>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      Title: "This Is the Home ",
+      pressed:false
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
