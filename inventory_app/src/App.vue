728e655cd13dd5e6d3b0cbd26aa1366d90345b6d
Muhammed Saad <muhammedsaad306@gmail.com>
2018-12-30 23:02:06 +0200

    adding router
    routing not working yet

--- inventory_app/src/App.vue
+++ inventory_app/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-
-  <div class="">
+    <div class>
       <b-navbar toggleable="md" type="dark" variant="dark">
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand href="#">Inventory </b-navbar-brand>
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
-          <b-nav-item href="#">Warehouses</b-nav-item>
-          <b-nav-item href="#">Products</b-nav-item>
-          <b-nav-item href="#">Customers</b-nav-item>
-          <b-nav-item href="#">Events</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
-
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">Inventory</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav>
+            <b-nav-item href="#">Warehouses</b-nav-item>
+            <b-nav-item href="#">Products</b-nav-item>
+            <b-nav-item href="#">Customers</b-nav-item>
+            <b-nav-item href="#">Events</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+     <router-view></router-view>
+    
   </div>
 </template>
 
