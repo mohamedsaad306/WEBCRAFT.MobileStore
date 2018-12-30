3b115d8deb410dff6a98c69269bc96619233af07
Muhammed Saad <muhammedsaad306@gmail.com>
2018-12-30 23:02:06 +0200

    adding router
    routing not working yet

--- inventory_app/src/main.js
+++ inventory_app/src/main.js
@@ -1,12 +1,29 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import BootstrapVue from "bootstrap-vue"
+
 import App from './App.vue'
+import products from'./components/products.vue'
+import HelloWorld from'./components/HelloWorld.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
+ 
 Vue.use(BootstrapVue)
 
-new Vue({
-  el: '#app',
+// const routes = [
+//   { path: '/home', component: App },
+//   { path: '/prroducts', component: products },
+//   { path: '/HelloWorld', component: HelloWorld },
+// ]
+// const router = new VueRouter({
+//   routes // short for `routes: routes`
+// })
+
+ 
+
+new Vue({  
+   el: '#app',
   render: h => h(App)
-})
+  // router
+}).$mount('#app')
