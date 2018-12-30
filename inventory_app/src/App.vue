f48b7f359f65fd5ab27e8fb6dcafeae9f0ca199a
Muhammed Saad <muhammedsaad306@gmail.com>
2018-12-30 21:38:15 +0200

    Adding bootstrap-vue
    success

--- inventory_app/src/App.vue
+++ inventory_app/src/App.vue
@@ -4,35 +4,56 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
     </ul>
+    <div>
+      <b-alert show dismissible>Dismissible Alert! Click the close button over there
+        <b>&rArr;</b>
+      </b-alert>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App New Inventory "
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -40,7 +61,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
