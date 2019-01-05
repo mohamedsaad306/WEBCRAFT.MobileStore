8091304b32311552771c294a9d819c818a85b061
Mohamed <Mohamed@MOHAMED-PC>
2019-01-05 14:39:03 +0200

    adding Vue

---
+++ inventory_app/src/components/Customers/Customers.Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>{{data1}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Customers.Main",
+  data() {
+    return {
+      data1: "Customers's main data "
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ inventory_app/src/components/Warehouses/Warehouses.Main.vue
@@ -0,0 +1,14 @@
+ <template>
+  <div>
+    <h3>Warehouses Main</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Warehouses.Main"
+};
+</script>
+
+<style>
+</style>
