91cad962629f66196584c8ffe7bbb129daab13ef
Mohamed <Mohamed@MOHAMED-PC>
2019-01-07 01:20:23 +0200

    load warehouse list with demo data
    adding watcher to selected warehouse

---
+++ inventory_app/src/components/Warehouses/Warehouses.List.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <b-table striped hover :items="items" :fields="fields">
+      <template slot="show_details" slot-scope="row">
+        <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
+        <b-button
+          size="sm"
+          @click.stop="viewProductsList(row)"
+          class="mr-2"
+          :variant="row.index == activeIndex ? 'danger':'dark'"
+        >Products</b-button>
+      </template>
+    </b-table>
+    <hr>
+    <!-- <div v-show=""></div> -->
+  </div>
+</template>
+
+  
+
+<script>
+import ProductsList from "../Products/Products.List.vue";
+export default {
+  name: "WarehoucesList",
+  components: {
+    ProductsList
+  },
+  methods: {
+    viewProductsList(r) {
+      console.log("Show");
+      r.index == this.activeIndex
+        ? (this.activeIndex = -1)
+        : (this.activeIndex = r.index);
+      console.log(r);
+      console.log(this.activeIndex);
+    }
+  },
+  data() {
+    return {
+      activeIndex: -1,
+      fields: [
+        { key: "name", label: "Location ", sortable: true },
+        { key: "value", label: "Items Value", sortable: true },
+        { key: "items_count", label: "Items Count", sortable: true },
+        "show_details"
+      ],
+      items: [
+        { name: "location one", value: 10560, items_count: 200 },
+        { name: "location Two ", value: 7500, items_count: 152 }
+      ]
+    };
+  },
+  computed: {
+    activeBtn(row) {
+      console.log("Show");
+      r.index == this.activeIndex
+        ? (this.activeIndex = -1)
+        : (this.activeIndex = r.index);
+      console.log(r);
+      console.log(this.activeIndex);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- inventory_app/src/components/Warehouses/Warehouses.Main.vue
+++ inventory_app/src/components/Warehouses/Warehouses.Main.vue
@@ -1,17 +1,20 @@
  <template>
   <div>
     <h3>Warehouses Main</h3>
-    <WarehouseForm></WarehouseForm>
+    <warehouses-list></warehouses-list>
+    <!-- <WarehouseForm></WarehouseForm> -->
   </div>
 </template>
 
 <script>
 import WarehouseForm from "./Warehouse.Form.vue";
+import WarehousesList from "./Warehouses.List.vue";
 
 export default {
   name: "WarehousesMain",
   components: {
-    WarehouseForm
+    WarehouseForm,
+    WarehousesList
   }
 };
 </script>
