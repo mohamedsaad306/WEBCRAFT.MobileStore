5f58943bb874cd3d9448f26d5d5b6e410297b061
Mohamed <Mohamed@MOHAMED-PC>
2019-01-07 19:46:47 +0200

    stuck in filter drop down list :-1:

---
+++ inventory_app/src/components/Shared/mydatalist.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <b-form-input list="items" v-model="selectedItemString" type="text"></b-form-input>
+    <b-form-input list="items" v-model="selectedItemString" type="text"></b-form-input>
+    <datalist id="items" v-show="false">
+      <option :value="item.id" v-for="item in itemsList" :key="item.id"></option>
+    </datalist>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyDataList",
+  data() {
+    return {
+      selectedItemString: ""
+    };
+  },
+  props: {
+    ["itemsList"]: Array
+  }
+};
+</script>
+
+<style>
+</style>
--- inventory_app/src/components/Warehouses/Warehouse.StockItems.vue
+++ inventory_app/src/components/Warehouses/Warehouse.StockItems.vue
@@ -1,30 +1,78 @@
 <template>
-  <b-table hover :items="stockItems" :fields="fields">
-    <template slot="actions" slot-scope="row">
-      <!-- A custom formatted footer cell  for field 'name' -->
-      <h3>asd</h3>
-    </template>
-  </b-table>
+  <div class="container">
+    <b-table hover :items="stockItems" :fields="fields">
+      <template slot="actions" slot-scope="row">
+        <!-- A custom formatted footer cell  for field 'name' -->
+        <b-button size="sm" variant="danger">Remove</b-button>
+      </template>
+      <template slot="table-caption">
+        <b-form-select
+          v-show="true"
+          list="products"
+          v-model="selectedProduct"
+          :data-id="selectedProductIndex"
+          class="custom-select"
+          :onselect="sayhi"
+          type="text"
+        >
+          <option
+            :data-id="item.id"
+            v-for="item in products"
+            :key="item.id"
+            :onSelect="sayhi"
+          >{{item.name}}</option>
+        </b-form-select>
+
+        <datalist id="products" v-show="false">
+          <option
+            :data-id="item.id"
+            v-for="item in products"
+            :key="item.id"
+            :onSelect="sayhi"
+          >{{item.name}}</option>
+        </datalist>
+
+        <b-button size="sm" variant="success">Addssss</b-button>
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
 export default {
   name: "WarehouseStockItem",
+  watch: {
+    selectedProduct: function() {
+      console.log(this.selectedProduct);
+    },
+    selectedProductIndex: function() {
+      console.log(this.selectedProductIndex);
+    }
+  },
+  methods: {
+    sayhi: function() {
+      alert("his");
+    }
+  },
   data() {
     return {
+      selectedProduct: "",
+      selectedProductIndex: "",
+      newItem: { productId: "", warehouseId: "", quantity: 0 },
       fields: {
-        warehouseId: {},
-        quantity: {},
+        // warehouseId: {},
         name: {
           key: "product.name"
         },
+        quantity: {},
         actions: {}
       },
 
       products: [
         { id: 1, name: "product 1", barcode: "123456" },
         { id: 2, name: "product 2", barcode: "654321" },
-        { id: 3, name: "product 3", barcode: "951753" }
+        { id: 3, name: "product 3", barcode: "951753" },
+        { id: 4, name: "product 3", barcode: "951753" }
       ],
 
       stockItems: [
